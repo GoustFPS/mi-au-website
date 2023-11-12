f0fcda1c09d4ea1d4aa93d10d1d3c4348eec90e8
dwdson <dwdson@gmail.com>
2023-11-12 16:29:40 -0300

    acréscimo das primeiras funcões e eventos em javascript

--- js/calculadora.js
+++ js/calculadora.js
@@ -49,14 +49,53 @@ const data = [
   //Funções
 
   function createTable(data) {
-       data.foreach((item) =>{
-        
-       })
-    
+       data.forEach((item) => {
+           const div = document.createElement("div");
+           div.classList.add("table-data");
+
+           const classification = document.createElement("p");
+           classification.innerText = item.classification;
+
+           const info = document.createElement("p");
+           info.innerText = item.info;
+
+           const obesity = document.createElement("p");
+           obesity.innerText = item.obesity;
+
+           div.appendChild(classification);
+           div.appendChild(info);
+           div.appendChild(obesity); 
+           
+           imcTable.appendChild(div);
+
+       });    
+  }
+  
+  function cleanInputs() {
+    heightInput.value = "";
+    weightInput.value = "";
+    imcNumber.className = "";
+    imcInfo.className = "";
   }
 
+  function validDigits(text) {
+    return text.replace(/[^0-9,]/g, "");
+  }
+  
 
   // Inicialização
+ createTable(data);
+
 
   // Eventos
+
+  [heightInput, weightInput].forEach(() => {
+
+  });
+
+  clearBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  
+    cleanInputs();
+  });
 
