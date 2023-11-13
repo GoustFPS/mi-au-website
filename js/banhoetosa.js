5a7dd86dcd93ba2f5c9c73ccb9ea8acd17b771ff
GoustFPS <jp.dutradutra@gmail.com>
2023-11-13 20:05:38 -0300

    alteração simples

--- js/banhoetosa.js
+++ js/banhoetosa.js
@@ -5,11 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
   const mainContainer = document.getElementById('main-container');
   const cadastroContainer = document.getElementById('cadastro-container');
   const cadastroForm = document.getElementById('cadastro-form');
-
   agendarBtn.addEventListener('click', function () {
     // Alternar a exibição entre a página principal e a página de cadastro
     mainContainer.style.display = 'none';
     cadastroContainer.style.display = 'block';
   });
-
- 
+})
