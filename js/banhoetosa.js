6393d2ece2c1ee617612f656acbf29a35b4b49f1
Itagicara-Jacauna <itagicara@gmail.com>
2023-11-13 02:37:43 -0300

    formulario banho e tosa

--- js/banhoetosa.js
+++ js/banhoetosa.js
@@ -12,18 +12,4 @@ document.addEventListener('DOMContentLoaded', function () {
     cadastroContainer.style.display = 'block';
   });
 
-  cadastroForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    // Lógica de processamento do formulário aqui
-    alert('Formulário enviado! Lógica de processamento pode ser adicionada aqui.');
-  });
-
-  function generateCupom() {
-    // Gere um cupom de desconto de forma aleatória ou use uma lógica específica
-    return 'TRIMESTRAL25'; // Por exemplo, um cupom fixo para o trimestre
-  }
-
-  const cupomValue = generateCupom();
-  cupomElement.textContent = cupomValue;
-});
-
+ 
